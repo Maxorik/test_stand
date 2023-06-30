c9f09001a0c753ef82e2d58dede0eefe40b5eaab
Your Name <gms441@mail.ru>
2023-06-30 21:49:43 +0700

    m

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const isMobile = typeof window.screen.orientation;
-const boo = typeof window.screen.orientation !== 'undefined'
-alert(isMobile, boo)
+const boo = typeof window.screen.orientation !== 'undefined';
+const j = JSON.stringify(isMobile);
+
+alert(boo);
+alert(j);
+alert(isMobile);
